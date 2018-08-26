d36259248324c1abf8fdced12d39c257028040c5
Santosh Kumar <sntshkmr60@gmail.com>
2018-08-26 10:00:31 +0530

    implement the menu

--- app.py
+++ app.py
@@ -2,19 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from database import Database
-# from models.post import Post
-from models.blog import Blog
+from menu import Menu
 
 Database.initialize()
 
-blog = Blog(author="Santosh",
-        title="Sample title",
-        description="Sample description")
+menu = Menu()
 
-blog.new_post()
-
-blog.save_to_mongo()
-
-from_database = Blog.from_mongo(blog.id)
-
-print(blog.get_posts)
+menu.run_menu()
